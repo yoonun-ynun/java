3731f4f0cffa31ddebc76f8beeb45467a062b1a9
yoonun-ynun <66334608+yoonun-ynun@users.noreply.github.com>
2020-06-03 05:48:32 +0900

    java
    test

---
+++ Hello2.java
@@ -0,0 +1,10 @@
+public class Hello2 {
+	public static void main (String[] args) {
+		System.out.println("Hello, world.");
+	}
+}
+class Hello3 {
+	public static void main (String[] args) {
+		System.out.println("world, Hello.");
+	}
+}
