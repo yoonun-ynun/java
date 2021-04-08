816c30ea0e47aa0d2b1f3b93859b80f6f4f40235
yoonun-ynun <66334608+yoonun-ynun@users.noreply.github.com>
2021-04-08 16:07:47 +0900

    hotel2
    hotel2

--- hotel2.java
+++ hotel2.java
@@ -1,10 +1,9 @@
 import java.util.*;
 class Cal{
-	int good;
-	int[] res = new int[good];
+	int[] res;
 	void a(){
-		for(int t=good;t>0;t--)
-			System.out.println(res[good-1]);
+		for(int t=res.length;t>0;t--)
+			System.out.println(res[t-1]);
 	}
 }
 class Main {
@@ -13,17 +12,17 @@ class Main {
 		Scanner scanner = new Scanner(System.in);
 		String input = scanner.nextLine();
 		int T = Integer.parseInt(input);
-		cal.good = T;
+		cal.res = new int [T];
 		int[] result = new int [T];
 		while(T>0){
 			int Hei = scanner.nextInt();
 			int Wei = scanner.nextInt();
 			int Num = scanner.nextInt();
+			int o = Num%Hei;
 			if(1<=Hei && Wei<=99 && 1<=Num && Num<=Hei*Wei){
 				int first = Num/Hei+1;
 				int last = Num%Hei*100;
 				result[T-1] = first + last;
-                System.out.println(result[T-1]);
 				cal.res[T-1] = result[T-1];
 			}
 			T--;
