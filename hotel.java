403781cb0bf72a48df44f79f054d5e17f9566094
yoonun-ynun <66334608+yoonun-ynun@users.noreply.github.com>
2021-04-08 12:00:48 +0900

    hotel
    hotel

--- hotel.java
+++ hotel.java
@@ -1,20 +1,12 @@
 import java.util.*;
-class Cal{
-	int good;
-	int[] res = new int[good];
-	void a(){
-		for(int t=good;t>0;t--)
-			System.out.println(res[good-1]);
-	}
-}
-class Main {
+class hotel {
 	public static void main(String[] args) {
-		Cal cal = new Cal();
+		int T;
 		Scanner scanner = new Scanner(System.in);
 		String input = scanner.nextLine();
-		int T = Integer.parseInt(input);
-		cal.good = T;
+		T = Integer.parseInt(input);
 		int[] result = new int [T];
+		num:
 		while(T>0){
 			int Hei = scanner.nextInt();
 			int Wei = scanner.nextInt();
@@ -23,11 +15,12 @@ class Main {
 				int first = Num/Hei+1;
 				int last = Num%Hei*100;
 				result[T-1] = first + last;
-                System.out.println(result[T-1]);
-				cal.res[T-1] = result[T-1];
+				System.out.println(result[T-1]);
+			}else {
+				System.out.println("불가능한 수식입니다.");
+				break num;
 			}
 			T--;
 		}
-		cal.a();
 	}
 }
