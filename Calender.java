fef4a90b435cf4a58c1a716be892d6c747cdf1cf
yoonun-ynun <66334608+yoonun-ynun@users.noreply.github.com>
2021-06-11 12:40:36 +0900

    Calender

---
+++ Calender.java
@@ -0,0 +1,32 @@
+import java.nio.file.attribute.AclEntry;
+import java.util.*;
+
+public class Calender {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("년도를 입력해 주세요>");
+        int year = sc.nextInt();
+        System.out.println("개월을 입력해 주세요>");
+        int month = sc.nextInt();
+        int START_DAY_OF_WEEK = 0;
+        int END_DAY = 0;
+        Calendar sDay = Calendar.getInstance();
+        Calendar eDay = Calendar.getInstance();
+
+        sDay.set(year, month-1, 1);
+        eDay.set(year, month, 1);
+        eDay.add(Calendar.DATE, -1);
+        START_DAY_OF_WEEK = sDay.get(Calendar.DAY_OF_WEEK);
+        END_DAY = eDay.get(Calendar.DATE);
+
+        System.out.println("      " + year + "년 " + month + "월");
+        System.out.println(" SU MO TU WE TH FR SA");
+        for(int i = 1;i<START_DAY_OF_WEEK;i++)
+            System.out.print("   ");
+        for(int i = 1, n=START_DAY_OF_WEEK;i<=END_DAY;i++, n++){
+            System.out.print((i<10)? "  "+i : " " +i);
+            if(n%7 == 0) System.out.println();
+        }
+
+    }
+}
