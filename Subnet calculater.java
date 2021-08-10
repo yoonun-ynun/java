b93b47912e6522715ec1d243bff166bbe194743c
yoonun-ynun <66334608+yoonun-ynun@users.noreply.github.com>
2021-08-10 13:52:52 +0900

    Update Subnet calculater.java

--- Subnet calculater.java
+++ Subnet calculater.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class Main {
+class Main {
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
         arraypharse ap = new arraypharse();
